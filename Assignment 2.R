08e6b19bfc67b3650b81f2eba7c4673eef608775
Steph Abro <stephabro@Stephs-MacBook-Pro.local>
2023-06-13 14:04:44 -0400

    first unedited version of code

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,11 +1,19 @@
 # Prompt the user to enter a three digit positive number
 armstrong <- readline(prompt = "Please enter a three digit positive number: ")
 armstrong <- as.integer(armstrong)
-# Check if the user input is numeric and an integer. If not, print an error message and quit.
+# Check if the user input is numeric, an integer, contains three characters, and is positive. If not, print an error message and quit.
 if (is.numeric(as.numeric(armstrong)) &
-    (as.numeric(armstrong)) %% 1 == 0) {
+    (as.numeric(armstrong)) %% 1 == 0 &
+    nchar(armstrong) = 3 &
+    as.numeric(armstrong) > 0) {
 # Check if the number is narcissistic 
-  if (armstrong 
+  if (((armstrong %% 100)^3) + (((armstrong %% 100) %% 10)^3) + ((armstrong %% 10)^3) == armstrong) {
+# Display the result with an appropriate message   
+     print(paste(armstrong, "is a Narcissitic number."))
+  } else {
+    print(paste(armstrong, "is not a Narcisstic number."))
+  }
+} else {
+  print("Oops silly goose. This entry is not valid. Terminating...")
 }
-# Check if the number is narcissistic 
 # Display the result with an appropriate message 
