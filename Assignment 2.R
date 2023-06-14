3ed44b46e656da67a181ecaf3ad9a623a9db88ed
Steph Abro <stephabro@Stephs-MacBook-Pro.local>
2023-06-13 21:18:46 -0400

    fourth FINAL version added lines to make sure the user doesn't input a value not between 100-999 (i.e. 001)

--- Assignment 2.R
+++ Assignment 2.R
@@ -6,14 +6,14 @@ if (is.numeric(as.numeric(armstrong)) & #numeric
     as.numeric(armstrong) > 0 & #positive number
     length(armstrong) == 3) { #three digits 
   armstrong <- as.integer(armstrong)
-# Check if the number is narcissistic 
+  # Check if the number is narcissistic 
   if (((armstrong %/% 100)^3) + (((armstrong %% 100) %/% 10)^3) + ((armstrong %% 10)^3) == armstrong) {
-# Display the result with an appropriate message   
-     print(paste(armstrong, "is a Narcissitic number."))
+    # Display the result with an appropriate message   
+    print(paste(armstrong, "is a Narcissitic number."))
   } else {
     print(paste(armstrong, "is not a Narcisstic number."))
   }
 } else {
   print("Oops, silly goose! This entry is not valid. Terminating...")
 }
- 
+
